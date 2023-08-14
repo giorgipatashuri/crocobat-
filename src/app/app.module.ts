959605143bbefa09f0e45ad62850b9a626ac6713
giorgipatashuri <patashuri2003@gmail.com>
2023-08-14 21:15:55 +0400

    added some modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/ui/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/ui/button/button.component';
+import { UiModule } from './components/ui/ui.module';
+import { HomeComponent } from './screens/home/home.component';
+import { TableComponent } from './components/table/table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    HomeComponent,
+    TableComponent,
   ],
   imports: [
+    UiModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
