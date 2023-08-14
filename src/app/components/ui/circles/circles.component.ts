e76d35eedc41d13eceeba22c7230fc6950f41686
giorgipatashuri <patashuri2003@gmail.com>
2023-08-14 22:57:18 +0400

    added circles animation

---
+++ src/app/components/ui/circles/circles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'animated-circules',
+  templateUrl: './circles.component.html',
+  styleUrls: ['./circles.component.scss']
+})
+export class CirclesComponent {
+
+}
