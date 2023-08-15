eaf24ba32459b0ba8aab1a00f5ce2f697b878d32
giorgipatashuri <patashuri2003@gmail.com>
2023-08-15 11:45:17 +0400

    added navigation

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { User } from 'src/app/interfaces/user.interface';
 import { ApiService } from 'src/app/services/api.service';
+import { RoutingService } from 'src/app/services/routing.service';
 
 @Component({
   selector: 'app-table',
@@ -8,14 +9,14 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-  constructor(private api:ApiService){}
+  constructor(private api:ApiService,private routing:RoutingService){}
   users:User[]=[]
 
 
   ngOnInit(){
     this.api.getUsers().subscribe((data)=>this.users=data)
   }
-  test(){
-    console.log(this.users)
+  navigateToUserDetail(userId:number){
+     this.routing.navigateToUserDetail(userId)
   }
 }
