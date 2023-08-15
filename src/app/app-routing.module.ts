977bcca593f58e7329484abce55a2d8af1f4d6f8
giorgipatashuri <patashuri2003@gmail.com>
2023-08-15 23:14:15 +0400

    added application routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './screens/home/home.component';
+import { UserDetailsComponent } from './screens/user-details/user-details.component';
+import { UserDetailsResolver } from './screens/user-details/user-details.resolver';
+import { UserPostsComponent } from './screens/user-posts/user-posts.component';
+import { UserPostsResolver } from './screens/user-posts/user-posts.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:HomeComponent,
+  },
+  { 
+    path:"user/:id",
+    component:UserDetailsComponent,
+    resolve: {
+      user: UserDetailsResolver 
+    }
+  },
+  {
+    path:"users/:id/posts",
+    component:UserPostsComponent,
+    resolve:{
+      userPosts:UserPostsResolver
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
