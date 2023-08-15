8c75210012f737fd51927c3bd23d8f723ff5d583
giorgipatashuri <patashuri2003@gmail.com>
2023-08-15 23:12:56 +0400

    improved routing service

--- src/app/services/routing.service.ts
+++ src/app/services/routing.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import {  Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoutingService {
-  constructor(private router:Router){}
+  constructor(private router:Router,private location: Location){}
+
+  navigateToUserPosts(userId: number) {
+    this.router.navigate(['users', userId, 'posts']);
+  }
+
+  navigateToUserDetail(userId:number){
+    this.router.navigate(['/user', userId]);
+  }
   navigateToHomePage(){
     this.router.navigate(['/']);
   }
 
   navigateToPreviousPage() {
-    this.router.navigate(['..']);
+    this.location.back()
   }
 }
